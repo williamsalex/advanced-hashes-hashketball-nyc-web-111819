18209d735287403354f0cc952ef761a706f3ff1b
Alexander Williams <williams_alex@protonmail.com>
2019-11-17 16:05:01 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,3 +1,3 @@
 def game_hash
-  hash = {:home = {:team_name, :colors, :players}, :away = {:team_name, :colors, :players}}
+  hash = {:home = {:team_name, :colors, :players = [{:player_name, :number, :shoe, :points, :rebounds, :assists, :steals, :blocks, :slam_dunks}]}, :away = {:team_name, :colors, :players}}
 end
