29244f58d35a496454bf20b688cbbb3596e72ae0
Alexander Williams <williams_alex@protonmail.com>
2019-11-17 16:34:53 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -33,8 +33,8 @@ end
 def team_colors(name)
   masterList = game_hash
   for x in masterList do
-    puts x
     if x[:team_name] == name
+      puts x[:team_name]
       return x[:colors]
     end
   end
